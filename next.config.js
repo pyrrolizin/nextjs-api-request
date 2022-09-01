e09bf70dd741c43bc3bc53607403cccb1f3b173b
pyrrolizin <piti.x@gmx.net>
2022-09-01 14:23:03 +0200

    feat: adding runtime config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    githuburl: 'https://github.com/pyrrolizin/',
+    githubrepourl: 'https://github.com/pyrrolizin/...'
+  },
 }
 
 module.exports = nextConfig
